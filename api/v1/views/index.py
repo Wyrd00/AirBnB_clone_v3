54fe7115003bf2c145fab79644405cc61141d20a
MelissaN <majajachiu@yahoo.com>
2018-08-21 03:35:59 +0000

    add a route that counts storage objs

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,7 @@
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
 
 @app_views.route("/status")
 def status():
@@ -7,3 +9,19 @@ def status():
         return JSON of OK status
     '''
     return jsonify({'status' : 'OK'})
+
+
+@app_views.route("/stats")
+def storage_counts():
+    '''
+        return counts of all classes in storage
+    '''
+    cls_counts = {
+        "amenities" : storage.count("Amenity"),
+        "cities" : storage.count("City"),
+        "places" : storage.count("Place"),
+        "reviews" : storage.count("Review"),
+        "states" : storage.count("State"),
+        "users" : storage.count("User")
+    }
+    return jsonify(cls_counts)
