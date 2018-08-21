d459f3e9b5e4b08ef24c1429bd0762bb48e4623c
MelissaN <majajachiu@yahoo.com>
2018-08-21 03:41:09 +0000

    add 404 not found error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def tear_down(self):
@@ -14,6 +16,12 @@ def tear_down(self):
     close query after each session
     '''
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 if __name__ == "__main__":
     try:
