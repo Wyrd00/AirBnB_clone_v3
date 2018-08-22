822e6ef8f742bcecd89f6656d65bf58cb61e7342
IOLevi <amy.tai0120@gmail.com>
2018-08-21 17:59:46 -0700

    name error

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -16,5 +16,5 @@ from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
-from api.v1.views.place import *
+from api.v1.views.places import *
 from api.v1.views.reviews import *
